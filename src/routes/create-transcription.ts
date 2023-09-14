ea4ef8e413e2b0dcbd11af19bd867a99b036a39a
Bruno <bruno@sthorm.io>
2023-09-14 02:51:42 -0300

    feat: handle with transcription

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,8 +1,12 @@
 import Elysia from "elysia";
 import { z } from "zod";
 import { db } from "../lib/db";
-import { createReadStream } from "fs";
 import { videos } from "../lib/db/schema";
+import { supabaseClient } from "../lib/supabase";
+import axios, { AxiosError } from "axios";
+import FormData from "form-data";
+import { resolve } from "path";
+import { createReadStream } from "fs";
 
 export const createTranscriptionRouter = new Elysia()
 createTranscriptionRouter.post('/videos/:videoId/transcription', async ({ params, body, set }) => {
@@ -18,34 +22,64 @@ createTranscriptionRouter.post('/videos/:videoId/transcription', async ({ params
 
     const { prompt } = bodySchema.parse(body)
 
-    const video = await db.query.videos.findFirst({ where: ({ id }, { eq }) => {
-        return eq(id, videoId)
-    }})
+    try {
+        
+        const video = await db.query.videos.findFirst({ where: ({ id }, { eq }) => {
+            return eq(id, videoId)
+        }})
 
 
-    if (!video) {
-        set.status = 404
-        return {
-            error: 'video not found'
+        if (!video) {
+            set.status = 404
+            return {
+                error: 'video not found'
+            }
         }
+
+        const videoPath = video.path
+
+        const { data } = await supabaseClient.storage.from('videos').createSignedUrl(videoPath, 60 * 60, { download: true })
+
+        const response = await fetch(data?.signedUrl as string)
+
+        if (!response.ok) {
+            set.status = 400
+            return {
+                error: 'error fetching video'
+            }
+        }
+
+        const tmpVideoPath = resolve(import.meta.dir, '..', 'tmp', videoPath)
+        await Bun.write(tmpVideoPath, response)
+        const audioStream = createReadStream(tmpVideoPath)
+        const form = new FormData()
+        form.append('file', audioStream)
+        form.append('model', 'whisper-1')
+        form.append('language', 'pt')
+        form.append('response_format', 'json')
+        form.append('temperature', 0)
+        form.append('prompt', prompt)
+
+        const responseOpenAi = await axios.post('https://api.openai.com/v1/audio/transcriptions', form, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+                ...form.getHeaders()
+            }
+        })
+
+        const transcription = responseOpenAi.data.text
+
+        await db.update(videos).set({ transcription })
+        return { transcription }
+    } catch (error) {
+        set.status = 400
+        console.log('Error on transcription', error)
+
+        if (error instanceof AxiosError) {
+            return { error: error.response?.data }
+        }
+        return { error: 'Error on transcription' }
     }
 
-    const videoPath = video.path
-    const audioReadStream = createReadStream(videoPath)
-
-    //TODO: call OpenAI library to transcribe audio
-    // const response = await openai.audio.transcriptions.create({
-    //     file: audioReadStream,
-    //     model: 'whisper-1',
-    //     language: 'pt',
-    //     response_format: 'json',
-    //     temperature: 0,
-    //     prompt,
-    // })
-
-    const transcription = 'response.text'
-
-    await db.update(videos).set({ transcription })
-
-    return { transcription }
 })

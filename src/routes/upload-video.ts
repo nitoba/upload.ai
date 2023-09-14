3808ce2ebe7ff16b2c2ab2dc52028dc13cb1c94b
Bruno <bruno@sthorm.io>
2023-09-13 23:37:30 -0300

    feat: save uploaded video

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -3,33 +3,35 @@ import { BunFile } from "bun";
 import { randomUUID } from "crypto";
 import { basename, extname } from "path";
 import { supabaseClient } from "../lib/supabase";
+import { db } from "../lib/db";
+import { videos } from "../lib/db/schema";
 
 export const uploadVideoRouter = new Elysia()
 
-uploadVideoRouter. post('/videos', async ({ request, set }) => {
-    const data = (await request.formData()).get('file') as BunFile
+uploadVideoRouter.post('/videos', async ({ request, set }) => {
+    const file = (await request.formData()).get('file') as BunFile
 
-    if (!data) {
+    if (!file) {
         set.status = 400
         return {
             error: 'Missing file.'
         }
     }
 
-    const extension = extname(data.name as string)
+    const extension = extname(file.name as string)
 
     if (extension !== '.mp3') {
         set.status = 400
         return { error: 'Invalid input type, please upload a MP3.' }
     }
 
-    const fileBaseName = basename(data.name as string, extension)
+    const fileBaseName = basename(file.name as string, extension)
     const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
 
-    const fileAsStream = data.stream()
+    const fileAsStream = file.stream()
 
     for await (const chunk of fileAsStream) {
-        const { error } = await supabaseClient.storage.from('videos').upload(fileUploadName, chunk, {
+        const { error, data } = await supabaseClient.storage.from('videos').upload(fileUploadName, chunk, {
             contentType: 'audio/mpeg',
             upsert: true,
         })
@@ -42,7 +44,15 @@ uploadVideoRouter. post('/videos', async ({ request, set }) => {
                 error: error.message
             }
         } else {
-            return { message: 'video uploaded with success!' }
+            const videoToInsert = {
+                id: randomUUID(),
+                name: file.name ?? fileUploadName,
+                path: data.path,
+            }
+
+            await db.insert(videos).values(videoToInsert)
+
+            return { message: 'video uploaded with success!', video: videoToInsert }
         }
     }
 })
